5e7ccdf73b8ffcbd6a3f62a468dddfad2cc54c73
yslcmozn@outlook.com <yslcmozn@outlook.com>
2020-10-31 08:58:48 +0300

    v1 hesapmak

---
+++ GithubHesapMak/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubHesapMak
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Sayi: ");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("2. Sayi: ");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("İslem Seçiniz +, -, *, / ");
+            string islem = Console.ReadLine();
+
+            switch (islem)
+            {
+                case "+":
+                    double sonuc = sayi1 + sayi2;
+                    Console.WriteLine("Sonuç: " + sonuc);
+                    break;
+                case "-":
+                    sonuc = sayi1 - sayi2;
+                    Console.WriteLine("Sonuç: " + sonuc);
+                    break;
+                case "*":
+                    sonuc = sayi1 * sayi2;
+                    Console.WriteLine("Sonuç: " + sonuc);
+                    break;
+                case "/":
+                    sonuc = sayi1 / sayi2;
+                    Console.WriteLine("Sonuç: " + sonuc);
+                    break;
+                default:
+                    Console.WriteLine("Hata");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
---
+++ HesapMakinesi/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1. Sayıyı Giriniz:");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("İşlem Belirtiniz: +, -, *, / ");
+            string islem = Console.ReadLine();
+            Console.Write("2. Sayıyı Giriniz:");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+
+            if (islem == "+")
+            {
+                double sonuc = sayi1 + sayi2;
+                Console.WriteLine("Sonuç: " + sonuc);
+            }
+            if (islem == "-")
+            {
+                double sonuc = sayi1 - sayi2;
+                Console.WriteLine("Sonuç: " + sonuc);
+            }
+            if (islem == "*")
+            {
+                double sonuc = sayi1 * sayi2;
+                Console.WriteLine("Sonuç: " + sonuc);
+            }
+            if (islem == "/")
+            {
+                if (sayi2 == 0)
+                {
+                    Console.WriteLine("Payda 0 olamaz.Lütfen başka bir işlem deneyin.");
+                }
+                else
+                {
+                    double sonuc = sayi1 / sayi2;
+                    Console.WriteLine("Sonuç: " + sonuc);
+                }
+                
+            }
+            Console.ReadLine();
+        }
+             
+    }
+}
+
+//if (sayi2 == 0)
+//{
+//    Console.WriteLine("Payda 0 olamaz.Lütfen başka bir işlem deneyin");
+//}
+//else
+//{
+//    double sonuc = sayi1 / sayi2;
+//    Console.WriteLine("Sonuç: " + sonuc);
